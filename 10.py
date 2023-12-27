a74a84245c08c6cff7f887433d101350f1956111
ankitg-02 <146383416+ankitg-02@users.noreply.github.com>
2023-12-27 18:37:38 +0530

    Add files via upload

---
+++ 10.py
@@ -0,0 +1,3 @@
+a=int(input("enter a number:"))
+m=float(a)
+print(m)
---
+++ 9.py
@@ -0,0 +1,6 @@
+import string
+c=input("enter string:")
+if(c.islower):
+    print("c:",c.upper())
+else:
+    print("c:",c.lower())
